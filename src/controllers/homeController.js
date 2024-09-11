9c509da1f72e007b8d13957c399038b16f1b409c
manh190502 <code.manh1905@gmail.com>
2024-09-11 16:10:39 +0700

    promise mysql

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,13 +8,30 @@ const getEjs = (req, res) => {
   res.render("sample");
 };
 
-const createUser = (req, res) => {
-  console.log(">>>", req.body);
-  res.send("create user");
+const createUser = async (req, res) => {
+  let email = req.body.email;
+  let name = req.body.name;
+  let city = req.body.city;
+
+  // console.log(">>>", email, name, city);
+
+  let [results, fields] = await connection.query(
+    `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+    [email, name, city]
+  );
+
+  console.log(">>> check results ", results);
+
+  res.send("Created user succeed !");
+};
+
+const getCreatePage = (req, res) => {
+  res.render("create.ejs");
 };
 
 module.exports = {
   getHomepage,
   getEjs,
   createUser,
+  getCreatePage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,12 +4,15 @@ const {
   getHomepage,
   getEjs,
   createUser,
+  getCreatePage,
 } = require("../controllers/homeController");
 const { get } = require("https");
 
 // router.method('/route', handler)
 router.get("/", getHomepage);
 router.get("/ejs", getEjs);
+
+router.get("/create", getCreatePage);
 
 router.post("/create-user", createUser);
 
