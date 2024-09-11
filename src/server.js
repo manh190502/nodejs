aa0d401cf975345d21c499de912218d1b58949df
manh190502 <code.manh1905@gmail.com>
2024-09-11 12:24:41 +0700

    connection database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 require("dotenv").config();
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -15,6 +16,19 @@ configViewEngine(app);
 // route
 app.use("/", webRoutes);
 
+// test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+// simple query
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log(">>>results ", results);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
